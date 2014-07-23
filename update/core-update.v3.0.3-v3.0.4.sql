f6f4b457f4e6111f0cf957afbdadf4c5ee35b2e1
jbe <jbe@localhost>
2014-07-23 21:27:50 +0200

    Delete authority_* information from members in functions "delete_member" and "delete_private_data"

--- update/core-update.v3.0.3-v3.0.4.sql
+++ update/core-update.v3.0.3-v3.0.4.sql
@@ -31,4 +31,128 @@ COMMENT ON COLUMN "session"."authority"         IS 'Temporary store for "member"
 COMMENT ON COLUMN "session"."authority_uid"     IS 'Temporary store for "member"."authority_uid" during member account creation';
 COMMENT ON COLUMN "session"."authority_login"   IS 'Temporary store for "member"."authority_login" during member account creation';
 
+CREATE OR REPLACE FUNCTION "delete_member"("member_id_p" "member"."id"%TYPE)
+  RETURNS VOID
+  LANGUAGE 'plpgsql' VOLATILE AS $$
+    BEGIN
+      UPDATE "member" SET
+        "last_login"                   = NULL,
+        "last_delegation_check"        = NULL,
+        "login"                        = NULL,
+        "password"                     = NULL,
+        "authority"                    = NULL,
+        "authority_uid"                = NULL,
+        "authority_login"              = NULL,
+        "locked"                       = TRUE,
+        "active"                       = FALSE,
+        "notify_email"                 = NULL,
+        "notify_email_unconfirmed"     = NULL,
+        "notify_email_secret"          = NULL,
+        "notify_email_secret_expiry"   = NULL,
+        "notify_email_lock_expiry"     = NULL,
+        "login_recovery_expiry"        = NULL,
+        "password_reset_secret"        = NULL,
+        "password_reset_secret_expiry" = NULL,
+        "organizational_unit"          = NULL,
+        "internal_posts"               = NULL,
+        "realname"                     = NULL,
+        "birthday"                     = NULL,
+        "address"                      = NULL,
+        "email"                        = NULL,
+        "xmpp_address"                 = NULL,
+        "website"                      = NULL,
+        "phone"                        = NULL,
+        "mobile_phone"                 = NULL,
+        "profession"                   = NULL,
+        "external_memberships"         = NULL,
+        "external_posts"               = NULL,
+        "statement"                    = NULL
+        WHERE "id" = "member_id_p";
+      -- "text_search_data" is updated by triggers
+      DELETE FROM "setting"            WHERE "member_id" = "member_id_p";
+      DELETE FROM "setting_map"        WHERE "member_id" = "member_id_p";
+      DELETE FROM "member_relation_setting" WHERE "member_id" = "member_id_p";
+      DELETE FROM "member_image"       WHERE "member_id" = "member_id_p";
+      DELETE FROM "contact"            WHERE "member_id" = "member_id_p";
+      DELETE FROM "ignored_member"     WHERE "member_id" = "member_id_p";
+      DELETE FROM "session"            WHERE "member_id" = "member_id_p";
+      DELETE FROM "area_setting"       WHERE "member_id" = "member_id_p";
+      DELETE FROM "issue_setting"      WHERE "member_id" = "member_id_p";
+      DELETE FROM "ignored_initiative" WHERE "member_id" = "member_id_p";
+      DELETE FROM "initiative_setting" WHERE "member_id" = "member_id_p";
+      DELETE FROM "suggestion_setting" WHERE "member_id" = "member_id_p";
+      DELETE FROM "membership"         WHERE "member_id" = "member_id_p";
+      DELETE FROM "delegation"         WHERE "truster_id" = "member_id_p";
+      DELETE FROM "non_voter"          WHERE "member_id" = "member_id_p";
+      DELETE FROM "direct_voter" USING "issue"
+        WHERE "direct_voter"."issue_id" = "issue"."id"
+        AND "issue"."closed" ISNULL
+        AND "member_id" = "member_id_p";
+      RETURN;
+    END;
+  $$;
+
+CREATE OR REPLACE FUNCTION "delete_private_data"()
+  RETURNS VOID
+  LANGUAGE 'plpgsql' VOLATILE AS $$
+    BEGIN
+      DELETE FROM "temporary_transaction_data";
+      DELETE FROM "member" WHERE "activated" ISNULL;
+      UPDATE "member" SET
+        "invite_code"                  = NULL,
+        "invite_code_expiry"           = NULL,
+        "admin_comment"                = NULL,
+        "last_login"                   = NULL,
+        "last_delegation_check"        = NULL,
+        "login"                        = NULL,
+        "password"                     = NULL,
+        "authority"                    = NULL,
+        "authority_uid"                = NULL,
+        "authority_login"              = NULL,
+        "lang"                         = NULL,
+        "notify_email"                 = NULL,
+        "notify_email_unconfirmed"     = NULL,
+        "notify_email_secret"          = NULL,
+        "notify_email_secret_expiry"   = NULL,
+        "notify_email_lock_expiry"     = NULL,
+        "notify_level"                 = NULL,
+        "login_recovery_expiry"        = NULL,
+        "password_reset_secret"        = NULL,
+        "password_reset_secret_expiry" = NULL,
+        "organizational_unit"          = NULL,
+        "internal_posts"               = NULL,
+        "realname"                     = NULL,
+        "birthday"                     = NULL,
+        "address"                      = NULL,
+        "email"                        = NULL,
+        "xmpp_address"                 = NULL,
+        "website"                      = NULL,
+        "phone"                        = NULL,
+        "mobile_phone"                 = NULL,
+        "profession"                   = NULL,
+        "external_memberships"         = NULL,
+        "external_posts"               = NULL,
+        "formatting_engine"            = NULL,
+        "statement"                    = NULL;
+      -- "text_search_data" is updated by triggers
+      DELETE FROM "setting";
+      DELETE FROM "setting_map";
+      DELETE FROM "member_relation_setting";
+      DELETE FROM "member_image";
+      DELETE FROM "contact";
+      DELETE FROM "ignored_member";
+      DELETE FROM "session";
+      DELETE FROM "area_setting";
+      DELETE FROM "issue_setting";
+      DELETE FROM "ignored_initiative";
+      DELETE FROM "initiative_setting";
+      DELETE FROM "suggestion_setting";
+      DELETE FROM "non_voter";
+      DELETE FROM "direct_voter" USING "issue"
+        WHERE "direct_voter"."issue_id" = "issue"."id"
+        AND "issue"."closed" ISNULL;
+      RETURN;
+    END;
+  $$;
+
 COMMIT;
